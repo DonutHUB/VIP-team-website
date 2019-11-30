c8914a8d267f92b1eabd2d9224396da75d049172
dyu63 <dyu63@gatech.edu>
2019-11-30 16:16:02 -0500

    Navbar links working, Title changes, css path fixed inside htmls

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
- 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,8 +13,19 @@ def homepage():
     except Exception(e):
         return str(e)
 
-@app.route('/about')
-def aboutpage():
+@app.route('/about.html')
+def aboutPage():
+
+    title = "About VIP Humor Genome"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("about.html", title=title, paragraph=paragraph, pageType=pageType)
+
+
+@app.route('/index.html')
+def homePage():
 
     title = "About this site"
     paragraph = ["blah blah blah memememememmeme blah blah memememe"]
@@ -23,8 +34,17 @@ def aboutpage():
 
     return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
 
+@app.route('/subteam.html')
+def subteamPage():
 
-@app.route('/about/contact')
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("subteam.html", title=title, paragraph=paragraph, pageType=pageType)
+
+@app.route('/contact.html')
 def contactPage():
 
     title = "About this site"
@@ -32,9 +52,7 @@ def contactPage():
 
     pageType = 'about'
 
-    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
+    return render_template("contact.html", title=title, paragraph=paragraph, pageType=pageType)
 
-
- 
 if __name__ == "__main__":
 	app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
